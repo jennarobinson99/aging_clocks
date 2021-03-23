7a931a8ca4a8fd06d0e2841cd57f0edf49d3e5b0
Jonas Rauchhaus <jr317@brc-cancer.med.ic.ac.uk>
2021-03-23 17:13:51 +0000

    automised workflow, read configuration meta data from file sh_scripts/config.sh

--- sh_scripts/CpG_G4_overlap.sh
+++ sh_scripts/CpG_G4_overlap.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 #Need to execute this script from project root directory aging_clocks/
-# Usage: sh sh_scripts/CpG_G4_overlap.sh input_file output_file window_size
+# Usage: sh sh_scripts/CpG_G4_overlap.sh CpG_file G4_file_plus G4_file_minus chain_file genome_file output_file window_size
+
 # first transform standardised CpG .csv file into .bed file
-input_file=$1
-output_file=$2
-window_size=$3
-echo $1
-echo $2
+CpG_file=$1 # file containing the CpG coordinates
+G4_file_plus=$2 # file containing plus strand of G4 coordinates
+G4_file_minus=$3 # file containing minus strand of G4 coordinates
+chain_file=$4 # chain file for lifting over between genome builds
+genome_file=$5 # bed file specifying shuffleling ranges for overlap
+output_file=$6 # desired output file name for fold enrichment results
+window_size=$7 # window size around CpGs 
+echo -e "Processing files..."
+echo -e "CpG file: \t $CpG_file"
+echo -e "G4 files: \t $G4_file_plus (plus), $G4_file_minus (minus)"
+echo -e "Chain file: \t $chain_file"
+echo -e "Genome file: \t $genome_file"
+echo -e "Output file: \t $output_file"
+echo -e "Window size: \t $window_size"
 echo "Transforming CpG format from .csv to .bed ..."
-Rscript --vanilla R_scripts/csv_to_bed.R $input_file  temp/CpGs_locs.bed
+Rscript --vanilla R_scripts/csv_to_bed.R $CpG_file temp/CpGs_locs.bed
 echo "... finished transforming."
 
 # next lift over CpG coordinates into horvath_CpGs_hg38
 echo "Lifting over CpGs into hg38..."
-Rscript --vanilla R_scripts/lift_over.R temp/CpGs_locs.bed temp/CpGs_lifted.bed data/chain_files/hg18ToHg38.over.chain
+Rscript --vanilla R_scripts/lift_over.R temp/CpGs_locs.bed temp/CpGs_lifted.bed $chain_file
 echo "... lifted CpGs."
 
 # Next filter coordinates (positive, negative correlation) and extend bases by window length n
@@ -25,22 +35,23 @@ echo "... filtered bases."
 
 # concatenate both strands of G4 data
 echo "Concatenating both strands of G4 file ..."
-cat data/G4_maps/G4s_human_minus.bed data/G4_maps/G4s_human_plus.bed > temp/G4s_human.bed
+cat $G4_file_plus $G4_file_minus > temp/G4s_human.bed
 echo "... concatenated G4 files."
 
 # Intersecting files
 echo "Finding G4s with overlap to CpG windows..."
-bedtools intersect -wa -a temp/G4s_human.bed -b temp/CpGs_ext.bed > out/G4s_in_CpGs.bed
-n_G4s=$(wc -l out/G4s_in_CpGs.bed | awk '{print $1}')
+name_G4_out_file="out/G4s_in_CpGs_ws_${window_size}.bed"
+bedtools intersect -wa -a temp/G4s_human.bed -b temp/CpGs_ext.bed > $name_G4_out_file
+n_G4s=$(wc -l $name_G4_out_file | awk '{print $1}')
 echo "Number of G4s found to lie within CpGs: $n_G4s"
 
 echo "Finding CpGs with overlap to G4s..."
-bedtools intersect -wa -a temp/CpGs_ext.bed -b temp/G4s_human.bed > out/CpGs_in_G4s.bed
-n_CpGs=$(wc -l out/CpGs_in_G4s.bed | awk '{print $1}')
+name_CpG_out_file="out/CpGs_in_G4s_ws_${window_size}.bed"
+bedtools intersect -wa -a temp/CpGs_ext.bed -b temp/G4s_human.bed > $name_CpG_out_file
+n_CpGs=$(wc -l $name_CpG_out_file | awk '{print $1}')
 echo "Number of CpGs found to lie within G4s: $n_CpGs"
 
-# Write overlap results to file
-
+# Assess Fold enrichment 
 echo "Control case: shuffle G4 and CpGs coordinates and overlap again..."
 # loop through multiple random seeds to get good value for control
 # get mean for CpGs and G4s
@@ -50,18 +61,18 @@ for i in 1 2 3
 do
 echo "Loop at i = $i"
 #Shuffle the data
-bedtools shuffle -i temp/G4s_human.bed -g data/genome_files/chromInfo.txt -seed $i > temp/G4s_human_shuffled.bed
-bedtools shuffle -i temp/CpGs_ext.bed -g data/genome_files/chromInfo.txt -seed $i > temp/CpGs_ext_shuffled.bed
+bedtools shuffle -i temp/G4s_human.bed -g $genome_file -seed $i > temp/G4s_human_shuffled.bed
+bedtools shuffle -i temp/CpGs_ext.bed -g $genome_file -seed $i > temp/CpGs_ext_shuffled.bed
 
 #calculate number of G4s that lie within shuffled CpGs
-bedtools intersect -wa -a temp/G4s_human.bed -b temp/CpGs_ext_shuffled.bed > out/G4s_in_shuffled_CpGs_temp.bed
-n_G4s_temp=$(wc -l out/G4s_in_shuffled_CpGs_temp.bed | awk '{print $1}')
+bedtools intersect -wa -a temp/G4s_human.bed -b temp/CpGs_ext_shuffled.bed > temp/G4s_in_shuffled_CpGs_temp.bed
+n_G4s_temp=$(wc -l temp/G4s_in_shuffled_CpGs_temp.bed | awk '{print $1}')
 # calculate running average with bc program
 mean_G4s=$(echo "scale=6;$mean_G4s+1/3*$n_G4s_temp" | bc)
 
 #calculate number of CpGs that lie within G4s
-bedtools intersect -wa -a temp/CpGs_ext.bed -b temp/G4s_human_shuffled.bed > out/CpGs_in_shuffled_G4s_temp.bed
-n_CpGs_temp=$(wc -l out/CpGs_in_shuffled_G4s_temp.bed | awk '{print $1}')
+bedtools intersect -wa -a temp/CpGs_ext.bed -b temp/G4s_human_shuffled.bed > temp/CpGs_in_shuffled_G4s_temp.bed
+n_CpGs_temp=$(wc -l temp/CpGs_in_shuffled_G4s_temp.bed | awk '{print $1}')
 # calcuate running average with bc program
 mean_CpGs=$(echo "scale=6;$mean_CpGs+1/3*$n_CpGs_temp" | bc)
 done
@@ -73,6 +84,6 @@ echo "G4s are enriched at CpG sites to a fold enrichment value of: $FE_G4s"
 echo "CpGs are enriched at G4 sites to a fold enrichment value of: $FE_CpGs"
 
 #write input_file, FE and window size to file 
-echo -e "$input_file; $window_size; $FE_G4s; $FE_CpGs" >> $output_file
+echo -e "$CpG_file; $window_size; $FE_G4s; $FE_CpGs" >> $output_file
 
 
--- sh_scripts/analyse_window_size.sh
+++ sh_scripts/analyse_window_size.sh
@@ -2,17 +2,22 @@
 
 # script takes in range of values for window size and input file
 # to execute CpG_G4_overlap.sh once for each case 
-# USAGE: sh sh_scripts/analyse_window_size.sh input_file output_file [window sizes...]
-#         e.g. sh sh_script/analyse_window_size.sh data/CpG_lists/Horvath_CpGs out/fold_enrichment.csv 10 100 1000 10000
-echo "$*"
-input_file=$1 
-output_file=$2
-shift
-shift 
+# USAGE: sh sh_scripts/analyse_window_size.sh (window sizes...]
+#         e.g. sh sh_script/analyse_window_size.sh 10 100 1000 10000
+
+# read in configuration data from sh_scripts/config.sh
+config_file="sh_scripts/config.sh"
+source $config_file
+#echo "CpG file: $CpG_file"
+#echo "G4 plus file: $G4_file_plus"
+#echo "G4 minus file: $G4_file_minus"
+#echo "Chain file: $chain_file"
+#echo "Genome file: $genome_file"
+#echo "Output file: $output_file"
 
 for window_size in "$@"; do
 	echo "Analysing overlap with window size = $window_size"
-	sh sh_scripts/CpG_G4_overlap.sh $input_file $output_file $window_size
+	sh sh_scripts/CpG_G4_overlap.sh $CpG_file $G4_file_plus $G4_file_minus $chain_file $genome_file $output_file $window_size
 done
 
 
