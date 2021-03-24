900f511862fcd2f63e38e99c2faaf067c207381c
Jonas <jonas.rauchhaus@gmail.com>
2021-03-24 11:01:24 +0000

    adjusted liftover for mouse genomes

--- R_scripts/lift_over.R
+++ R_scripts/lift_over.R
@@ -1,19 +1,5 @@
 #Usage: Rscript --vanilla lift_over.R input_file output_file chain_file
 
-library(rtracklayer)
-
-# FUNCTIONS
-# function to convert numeric chromosome number to .bed file chromosome string (e.g.:"chr1")
-chr_string <- function(number) {
-  if (is.integer(number)){
-    char <- as.character(number)
-    chromosome = paste("chr", char, sep = "")
-  }
-  else 
-    chromosome = NaN
-  return(chromosome)
-}
-
 # MAIN 
 
 # get arguments from the command line
@@ -31,20 +17,31 @@ if (length(args)!=3) {
 #input_file <- "temp/CpGs_locs.bed"
 #output_file <- "CpGs_lifted.bed"
 #chain_file <- "data/chain_files/hg18ToHg38.over.chain"
-
-chain <- import.chain(chain_file)
-
-#load the bed file containing genome coordinates 
-coordinates <- read.csv(input_file, header=F, col.names=c("chr", "start", "end", "weight"), sep='\t')
-
-#coerce dataframe into GRanges
-ranges <- makeGRangesFromDataFrame(coordinates)
-
-# use R implementation of Lift Over tool to convert ranges to hg38 coordinate system
-hg38_ranges <- liftOver(ranges, chain)
-# get the object in the right format and write to file
-hg38_coordinates <- data.frame(iranges=hg38_ranges)
-hg38_coordinates <- data.frame(cbind.data.frame(coordinates$chr, hg38_coordinates$iranges.start, 
-                          hg38_coordinates$iranges.end, coordinates$weight))
-names(hg38_coordinates) <- c("chr", "start", "end", "weight")
-write.table(hg38_coordinates, file=output_file, sep="\t", col.names = F, row.names = F, quote = F)
+if (chain_file=="-" | chain_file=="" | chain_file=="/" | chain_file=="\\") {
+  #load the bed file containing genome coordinates 
+  coordinates <- read.csv(input_file, header=F, col.names=c("chr", "start", "end", "weight"), sep='\t')
+  write.table(coordinates, file=output_file, sep="\t", col.names = F, row.names = F, quote = F)
+  print("No lift over requested, proceeding without it.")
+  
+} else {
+  
+  library(rtracklayer)
+  
+  
+  chain <- import.chain(chain_file)
+  
+  #load the bed file containing genome coordinates 
+  coordinates <- read.csv(input_file, header=F, col.names=c("chr", "start", "end", "weight"), sep='\t')
+  
+  #coerce dataframe into GRanges
+  ranges <- makeGRangesFromDataFrame(coordinates)
+  
+  # use R implementation of Lift Over tool to convert ranges to hg38 coordinate system
+  hg38_ranges <- liftOver(ranges, chain)
+  # get the object in the right format and write to file
+  hg38_coordinates <- data.frame(iranges=hg38_ranges)
+  hg38_coordinates <- data.frame(cbind.data.frame(coordinates$chr, hg38_coordinates$iranges.start, 
+                            hg38_coordinates$iranges.end, coordinates$weight))
+  names(hg38_coordinates) <- c("chr", "start", "end", "weight")
+  write.table(hg38_coordinates, file=output_file, sep="\t", col.names = F, row.names = F, quote = F)
+}
