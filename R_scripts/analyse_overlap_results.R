97b617382edd0806ed90359fa2be7ddce6ad9319
Jonas <jonas.rauchhaus@gmail.com>
2021-03-25 16:09:23 +0000

    reverted some changes in analyse_overlap_results.R

--- R_scripts/analyse_overlap_results.R
+++ R_scripts/analyse_overlap_results.R
@@ -4,17 +4,18 @@ library(scales)
 # get arguments from the command line
 args <- commandArgs(trailingOnly = TRUE)
 # test for correct arguments
-if (length(args)!=6) {
-  stop("Six arguments need to be supplied: FE_file CpG_bed_file G4_bed_file figure_name_g4 figure_name_CpGs figure_name_CpG_distribution", call.=TRUE)
+if (length(args)!=8) {
+  stop("Eight arguments need to be supplied: FE_file CpG_bed_file G4_bed_file CGI_map_file figure_name_g4 figure_name_CpGs figure_name_CpG_distribution figure_name_CGI_context", call.=TRUE)
 } else {
   # define all file names and parameters
   FE_file <- args[1]
   CpG_bed_file <- args[2]
   G4_bed_file <- args[3]
   CGI_map_file <- args[4]
-  figure_name_G4 <- args[4]
-  figure_name_CpGs <- args[5]
-  figure_name_chr_dist <- args[6]
+  figure_name_G4 <- args[5]
+  figure_name_CpGs <- args[6]
+  figure_name_chr_dist <- args[7]
+  figure_name_CGI_context <- args[8]
 }
 
 # # Read in the overlapped CpG and G4 data 
@@ -53,19 +54,24 @@ CpGs_neg <- CpGs_oI %>% filter(weight < 0) %>% mutate(correlation = "Negative")
 CpGs_oI <- rbind(CpGs_pos, CpGs_neg)
 
 # 3. Check which CpGs lie within CGIs
-intersected_file_name <- "temp/CpG_info.bed"
+intersected_file_name <- "temp/CpG_CGI_intersect.bed"
 command <- paste("bedtools intersect -wa -a", CpG_bed_file, "-b", CGI_map_file, ">", intersected_file_name, sep=" ")
 cat(command, "\n")
 try(system(command))
 CpGs_in_CGI <- read.csv(intersected_file_name, header=F, sep="\t", col.names=c("chr", "start", "end", "weight", "correlation"))
-CpGs_unmethylated <- CpGs_oI %>% filter(weight %in% CpGs_in_CGI[,"correlation"])%>% mutate(CGI = "Yes")
-CpGs_methylated <- CpGs_oI %>% filter(!(weight %in% CpGs_in_CGI[,"correlation"])) %>% mutate(CGI = "No")
+CpGs_unmethylated <- CpGs_oI %>% filter(weight %in% CpGs_in_CGI[,"weight"]) %>% mutate(CGI = "within CGI")
+CpGs_methylated <- CpGs_oI %>% filter(!(weight %in% CpGs_in_CGI[,"weight"])) %>% mutate(CGI = "outside CGI")
 CpGs_oI <- rbind(CpGs_unmethylated, CpGs_methylated) 
-write.table(CpGs_oI, file="out/unmethlyated.bed", sep="\t",quote=F)
+write.table(CpGs_oI, file="out/CpG_info.bed", sep="\t",quote=F)
 
 # plot CpG distribution 
 ggplot(CpGs_oI) + 
   geom_bar(aes(x=chr)) + 
-  facet_grid(correlation~CGI)
+  facet_grid(correlation~.)
 ggsave(figure_name_chr_dist)
+# plot CGI context
+ggplot(CpGs_oI) + 
+  geom_bar(aes(x=chr)) + 
+  facet_grid(CGI~.)
+ggsavve(figure_name_CGI_context)
 
