35a7c12efd5abfbff4833ec98a56a7a4d97408a2
Jonas <jonas.rauchhaus@gmail.com>
2021-04-11 14:06:29 +0100

    tried merging of subsampled sequences

--- R_scripts/find_sequences.R
+++ R_scripts/find_sequences.R
@@ -1,28 +1,33 @@
 setwd("D:/proj_epigen/aging_clocks")
 
 #load the bed file containing genome coordinates
-input_file <- "data/CpG_lists/all_CpGs_mm10_ws_1000_merged.bed"
+input_file <- "data/CpG_lists/all_CpGs_hg19_ws_50_merged.bed"
 coordinates <- read.csv(input_file, header=F, col.names=c("chr", "start", "end"), sep='\t')
 
 #import biostrings and genome
 library(Biostrings)
-library(BSgenome.Mmusculus.UCSC.mm10)
+library(BSgenome.Hsapiens.UCSC.hg19)
 library(seqinr)
-library(rGADEM)
+genome <- BSgenome.Hsapiens.UCSC.hg19
+
+# random subsampling to reduce amount of sequences
+length <-  dim(coordinates)[[1]]
+rand_idx <- sample(1:length, 500000)
+rand_sample <- coordinates[rand_idx,]
+
+# reducing doesnt seem to effect the sample -> already non-overlapping
+# sample_ranges <- makeGRangesFromDataFrame(rand_sample, keep.extra.columns = T)
+# sample_ranges <- reduce(sample_ranges)
 
 # get sequence from genome
-chromosomes <-coordinates[,"chr"]
-starts <- coordinates[,"start"]
-ends <- coordinates[,"end"]
+chromosomes <-rand_sample[,"chr"]
+starts <- rand_sample[,"start"]
+ends <- rand_sample[,"end"]
 sequences <- as.list(unname(Biostrings::getSeq(genome, chromosomes, starts, ends, as.character=T)))
 # create unique identifiers for each sequence
 ids <- c(1:length(chromosomes))
 id_string <- paste("Sequence_", as.character(ids), sep="")
 identifiers <- paste(id_string,chromosomes,sep="|")
-
+  
 # construct new fasta file with sequences
-write.fasta(sequences, identifiers, "sequences/all_CpGs_human.fasta", open="w", as.string=T)
-
-# run motif discovery
-ranges <- makeGRangesFromDataFrame(coordinates)
-gadem <- GADEM(ranges, verbose=1, genome=Mmusculus)
+write.fasta(sequences, identifiers, "sequences/all_CpGs_human_subsampled.fasta", open="w", as.string=T)
