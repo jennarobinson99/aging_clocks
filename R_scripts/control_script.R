647a955e9a7570ea1cf8c3b8dfaef3552d32d6fb
Jonas <jonas.rauchhaus@gmail.com>
2021-04-21 14:53:38 +0100

    pipeline implemented in R scripts (only plot_results() not working)

--- R_scripts/control_script.R
+++ R_scripts/control_script.R
@@ -3,7 +3,6 @@
 # IMPORTS 
 library(dplyr)
 library(rjson)
-library(stringr)
 
 # FUNCTIONS
 source("R_scripts/functions.R")
@@ -18,50 +17,19 @@ genome_file <- params[["genome_file"]]
 window_size_CpG_dist_plot <- params[["window_size_CpG_dist_plot"]]
 all_CpGs_file <- params[["all_CpGs_file"]]
 window_sizes <- params[["window_sizes"]]
-window_size <- window_sizes[1]
 
-# 1) Convert CpG data from .csv to .bed format
-# load data
-CpG_locs <- read.csv(CpG_file, header = TRUE, sep = ";")
-# get chromosomes vector in .bed file format
-chr_list <- list(CpG_locs[,"Chr"])
-chromosomes <- lapply(chr_list, chr_string)
-# get start end end positions
-starts <-  CpG_locs[, "MapInfo"]
-ends <-  CpG_locs[, "MapInfo"] + 1
-coefficients <- CpG_locs[,"CoefficientTraining"]
-# create data frame with .bed file appropriate format
-CpG_locs <-  cbind(data.frame(chromosomes), starts, ends, coefficients)
-names(CpG_locs) <- c("chromosome", "start", "end", "coefficient")
+# 1) Load G4 data and catenate both strands
+G4_locs <- load_G4_data(G4_file_plus, G4_file_minus)
 
+# 2) Load CpG data and convert from .csv to .bed format
+CpG_locs <- load_CpG_data(CpG_file = CpG_file)
 
-# 2) Lift over coordinates 
-if (chain_file=="-" | chain_file=="" | chain_file=="/" | chain_file=="\\") {
-  #load the bed file containing genome coordinates 
-  print("No lift over requested, proceeding without it.")
-} else {
-  library(rtracklayer)
-  #import chain file
-  chain <- import.chain(chain_file)
-  #coerce dataframe into GRanges
-  ranges <- makeGRangesFromDataFrame(CpG_locs)
-  # use R implementation of Lift Over tool to convert ranges to hg38 coordinate system
-  lifted_ranges <- liftOver(ranges, chain)
-  # get the object in the right format and write to file
-  temp_df <- data.frame(iranges=lifted_ranges)
-  CpG_locs <- data.frame(cbind.data.frame(CpG_locs$chromosome, temp_df$iranges.start, 
-                                                  temp_df$iranges.end, CpG_locs$coefficient))
-  names(CpG_locs) <- c("chromosome", "start", "end", "coefficient")
-}
+# 3) Lift over coordinates 
+CpG_locs <- lift_over(coordinates=CpG_locs, chain_file=chain_file)
 
+# 4) Analyse window size
+results <- analyse_window_size(query=CpG_locs, search_set=G4_locs, genome_file=genome_file, window_sizes=window_sizes)
 
-# 3) Extend bases
-CpG_locs <- CpG_locs %>% mutate(start=start-(window_size/2), end=end+(window_size/2)-1)
+# 5) Plot results 
+plot_results(overlap_results=results, query_name="CpGs", search_set_name="G4s")
 
-
-# 4) Load G4 data and catenate both strands
-G4_plus <- read.csv(G4_file_plus, header=F, sep="\t")
-G4_minus <- read.csv(G4_file_minus, header=F, sep="\t")
-G4_locs <- rbind.data.frame(G4_plus, G4_minus)
-names(G4_locs) <- c("chromosome", "start", "end", "score")
-G4_locs <- G4_locs %>% arrange(chromosome, start, end)
--- R_scripts/functions.R
+++ R_scripts/functions.R
@@ -1,13 +1,161 @@
 # This file contains all functions needed for overlap analysis. 
 
-overlap <- function(A, B){
-  # A and B should be Ranges objects which can be reduced and overlapped
+
+overlap <- function(query=NULL, search_set=NULL){
+  ### A and B should be Ranges objects which can be reduced and overlapped (subset overlap, bedtools -wa mode)
+  library(GenomicRanges)
+  ranges_A <- makeGRangesFromDataFrame(query, keep.extra.columns = T)
+  ranges_B <- makeGRangesFromDataFrame(search_set, keep.extra.columns = T)
+
+  # subset overlap mode: giving subset of query that is overlapping
+  overlap_ranges <- subsetByOverlaps(ranges_A, ranges_B) #returns subset of ranges_A which has overlap to ranges_B
+  red_overlap_ranges <- GenomicRanges::reduce(overlap_ranges) # merges potential duplicates
+  # TODO: need to keep extra columns (e.g. coefficient / score) -> for duplicates, take average
+  
+  # convert to data frame, return resulting dataframe
+  overlap_coor <- GRanges_to_df(ranges=red_overlap_ranges)
+  
+  return(overlap_coor)
+}
+
+
+analyse_overlap <- function(query = NULL, search_set = NULL, genome_file=NULL, random_trials=5) {
+  ### Function overlaps two files and analyses the overlap
+  
+  library(regioneR)
+  library(IRanges)
+  library(Biostrings)
+  library(stats)
+  
+  # Overlap the file and get query entries that are in the search set
+  overlap_coor <- overlap(query=query, search_set = search_set)
+  overlap_ranges <- makeGRangesFromDataFrame(overlap_coor)
+  num_overlaps <- dim(overlap_coor)[1]
+  
+  # Load and process genome file
+  genome <- read.csv(genome_file, header = F,sep="\t", col.names = c("chromosome", "length", "unknown"))
+  genome <- cbind.data.frame(genome$chromosome, 1, genome$length)
+  names(genome) <- c("chromosome", "start", "end")
+  
+  # Random shuffling overlap 
+  rand_overlaps <- vector()
+  for(i in 1:random_trials){
+    set.seed(i)
+    shuffled_coor <- GRanges_to_df(randomizeRegions(query, genome=genome))
+    shuffled_overlap <- overlap(query=shuffled_coor, search_set = search_set)
+    rand_overlaps[i] <- dim(shuffled_overlap)[1]
+  }
+  average_rand_overlaps <- mean(rand_overlaps)
+  
+  # Calculate fold enrichment
+  if(average_rand_overlaps == 0) {
+    print("Unable to calculate enrichment, zero random overlaps found. ")
+  } else {
+    enrichment <- num_overlaps / average_rand_overlaps
+  }
+  
+  # Calculate p-value against the whole genome (using hypergeometric dist)
+  num_bases <- sum(genome$end)
+  num_search_set_bases <- sum(search_set$end - search_set$start)
+  num_query_bases <- sum(query$end - query$start)
+  num_overlap_bases <- sum(overlap_coor$end - overlap_coor$start)
+  p_value <- phyper(q = num_overlap_bases, m = num_search_set_bases, n = (num_bases-num_search_set_bases), k = num_query_bases, lower.tail = F)
+  
+  return(list("overlap_coordinates" = overlap_coor, "num_overlaps" = num_overlaps, "fold_enrichment" = enrichment, "p_value" = p_value))
+}
+
+
+analyse_window_size <- function(query=NULL, search_set=NULL, genome_file=NULL, window_sizes=NULL){
+  ### Function loops through all given window sizes and calculates statistical metrics about distribution and enrichment for all. Outputs dataframe containing all results and produces graphs. 
+  
+  library(dplyr)
+  i <- 1
+  num_overlaps <- vector()
+  fold_enrichment <- vector()
+  p_value <- vector()
+  for(window_size in window_sizes){
+    query_ext <- query %>% mutate(start=start-round(window_size/2), end=end+round(window_size/2)-1)
+    temp_results <- analyse_overlap(query=query_ext, search_set = search_set, genome_file=genome_file)
+    num_overlaps[i] <- temp_results["num_overlaps"]
+    fold_enrichment[i] <- temp_results["fold_enrichment"]
+    p_value[i] <-  temp_results["p_value"]
+    i <- i + 1
+  }
+  
+  results <- cbind.data.frame(window_sizes, as.matrix(num_overlaps), as.matrix(fold_enrichment, digits=4), as.matrix(p_value))
+  names(results) <- c("window_size", "num_overlaps", "fold_enrichment", "p_value")
+  return(results)
+}
+
+
+load_CpG_data <- function(CpG_file=NULL){
+  ### Function loads csv data into dataframe and returns it
+  
+  # load data
+  CpG_locs <- read.csv(CpG_file, header = TRUE, sep = ";")
+  # get chromosomes vector in .bed file format
+  chr_list <- list(CpG_locs[,"Chr"])
+  chromosomes <- lapply(chr_list, chr_string)
+  # get start end end positions
+  starts <-  CpG_locs[, "MapInfo"]
+  ends <-  CpG_locs[, "MapInfo"] + 1
+  coefficients <- CpG_locs[,"CoefficientTraining"]
+  # create data frame with .bed file appropriate format
+  CpG_locs <-  cbind(data.frame(chromosomes), starts, ends, coefficients)
+  names(CpG_locs) <- c("chromosome", "start", "end", "coefficient") 
+  return(CpG_locs)
+}
+
+
+lift_over <- function(coordinates=NULL, chain_file=NULL) {
+  ### Function lifts over genome coordinates from one assembly to another given a chain file
+  
+  if (chain_file=="-" | chain_file=="" | chain_file=="/" | chain_file=="\\" | is.null(chain_file) ) {
+    #load the bed file containing genome coordinates 
+    print("No lift over requested, proceeding without it.")
+  } else {
+    library(rtracklayer)
+    #import chain file
+    chain <- import.chain(chain_file)
+    #coerce dataframe into GRanges
+    ranges <- makeGRangesFromDataFrame(coordinates)
+    # use R implementation of Lift Over tool to convert ranges to hg38 coordinate system
+    lifted_ranges <- liftOver(ranges, chain)
+    # get the object in the right format and write to file
+    temp_df <- data.frame(iranges=lifted_ranges)
+    lifted_coor <- data.frame(cbind.data.frame(coordinates$chromosome, temp_df$iranges.start, 
+                                            temp_df$iranges.end, coordinates$coefficient))
+    names(lifted_coor) <- c("chromosome", "start", "end", "coefficient")
+    return(lifted_coor)
+  }
+}
+
+
+load_G4_data <- function(G4_file_plus=NULL, G4_file_minus=NULL){
+  ### Function loads in given G4 data
+  library(dplyr)
+  G4_plus <- read.csv(G4_file_plus, header=F, sep="\t")
+  G4_minus <- read.csv(G4_file_minus, header=F, sep="\t")
+  G4_locs <- rbind.data.frame(G4_plus, G4_minus)
+  names(G4_locs) <- c("chromosome", "start", "end", "score")
+  G4_locs <- G4_locs %>% arrange(chromosome, start, end) 
+  G4_locs <- G4_locs %>% filter(chromosome!="chrX" & chromosome!="chrY" & chromosome!="chrM")
+  return(G4_locs)
+}
+
+
+GRanges_to_df <- function(ranges=NULL){
+  ### Function converts GRanges object to data frame
+  df <- data.frame(iranges=ranges)
+  df <- data.frame(cbind.data.frame(df$iranges.seqnames, df$iranges.start, df$iranges.end))
+  names(df) <- c("chr", "start", "end")
+  return(df)
 }
 
 
 chr_string <- function(number) {
-  # function to convert numeric chromosome number to .bed file chromosome string (e.g.:"chr1")
-  
+  ## Function to convert numeric chromosome number to .bed file chromosome string (e.g.:"chr1")
+  library(stringr)
   if (is.integer(number)){
     char <- as.character(number)
     chromosome = paste("chr", char, sep = "")
